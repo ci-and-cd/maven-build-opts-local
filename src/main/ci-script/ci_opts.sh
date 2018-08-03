2811134869e62f18373ec3ca6d37c9583f69a9de
haolun <haolun@example.com>
2018-08-04 01:05:03 +0800

    update options

--- src/main/ci-script/ci_opts.sh
+++ src/main/ci-script/ci_opts.sh
@@ -2,16 +2,18 @@
 
 ### CI OPTS ABOUT INFRASTRUCTURE BEGIN
 if [ -z "${CI_INFRA_OPT_PRIVATE_GIT_PREFIX}" ]; then CI_INFRA_OPT_OPENSOURCE_GIT_PREFIX="http://gitlab"; fi
+
 if [ -z "${CI_INFRA_OPT_DOCKER_REGISTRY_URL}" ]; then CI_INFRA_OPT_DOCKER_REGISTRY_URL="http://nexus3:5000/v2/"; fi
+
 if [ -z "${CI_INFRA_OPT_NEXUS3}" ]; then CI_INFRA_OPT_NEXUS3="http://nexus3:28081"; fi
 if [ -z "${CI_INFRA_OPT_SONAR_HOST_URL}" ]; then CI_INFRA_OPT_SONAR_HOST_URL="http://sonarqube:9000"; fi
 ### CI OPTS ABOUT INFRASTRUCTURE END
 
 ### CI OPTS BEGIN
 if [ -z "${CI_OPT_CHECKSTYLE_CONFIG_LOCATION}" ]; then CI_OPT_CHECKSTYLE_CONFIG_LOCATION="${CI_OPT_MAVEN_BUILD_REPO}/src/main/checkstyle/google_checks_8.10.xml"; fi
-#if [ -z "${CI_OPT_FRONTEND_NODEDOWNLOADROOT}" ]; then CI_OPT_FRONTEND_NODEDOWNLOADROOT="https://npm.taobao.org/mirrors/node/"; fi
+# default: https://nodejs.org/dist/  taobao: https://npm.taobao.org/mirrors/node/"
 #if [ -z "${CI_OPT_FRONTEND_NODEDOWNLOADROOT}" ]; then CI_OPT_FRONTEND_NODEDOWNLOADROOT="http://nexus3:28081/nexus/repository/node-dist/"; fi
-#if [ -z "${CI_OPT_FRONTEND_NPMDOWNLOADROOT}" ]; then CI_OPT_FRONTEND_NPMDOWNLOADROOT="http://registry.npm.taobao.org/npm/-/"; fi
+# default: http://registry.npm.taobao.org/npm/-/"  taobao: http://registry.npm.taobao.org/npm/-/
 #if [ -z "${CI_OPT_FRONTEND_NPMDOWNLOADROOT}" ]; then CI_OPT_FRONTEND_NPMDOWNLOADROOT="http://nexus3:28081/nexus/repository/npm-public/npm/-/"; fi
 if [ -z "${CI_OPT_PMD_RULESET_LOCATION}" ]; then CI_OPT_PMD_RULESET_LOCATION="${CI_OPT_MAVEN_BUILD_REPO}/src/main/pmd/pmd-ruleset-6.4.0.xml"; fi
 ### CI OPTS END
